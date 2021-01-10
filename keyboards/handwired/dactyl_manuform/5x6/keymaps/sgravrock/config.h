01cfe17313d8cf4ce0c09f579ee076dd99a70d2c
Steve Gravrock <sdg@panix.com>
2021-01-09 16:04:05 -0800

    Elite-c specific configuration
    Use the standard matrix pins and move the LEDs to a pin that the Pro Micro
    doesn't have.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/sgravrock/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/sgravrock/config.h
@@ -26,14 +26,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define EE_HANDS
 // Rows are doubled-up
 
-// Use D2 instead of B5 so we can use B5 for LEDs
-#undef MATRIX_COL_PINS
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, D2 }
-
-
 // Any of B5, B6, and C6 should work as long as they aren't
-// also used in the switch matrix. B7 is documented to work
-// but doesn't exist on the Pro Micro.
-#define BACKLIGHT_PIN B5
+// also used in the switch matrix. B7 is present on the Elite-C
+// but not the Pro Micro. To use LEDs on the Pro Micro, set
+// BACKLIGHT_PIN to B5 and redefine MATRIX_COL_PINS to use
+// D2 instead of B5. (That configuration will also work for
+// the Elite-C, but the standard matrix pins make things a
+// bit tidier.)
+#define BACKLIGHT_PIN B7
 #define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 2
