0b2068fa1ef9d2d7ae58a2a8619194964308f455
Steve Gravrock <sdg@panix.com>
2021-01-11 22:31:35 -0800

    Removed lower layer and added `/~

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/sgravrock/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/sgravrock/keymap.c
@@ -5,11 +5,9 @@
 
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _RAISE 1
 
 #define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -19,26 +17,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
      KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
-                      LOWER, TT(_RAISE) ,                                                        KC_LBRC, KC_RBRC,
+                      KC_GRAVE,TT(_RAISE) ,                                                        KC_LBRC, KC_RBRC,
                                                       // Outboard thumb keys
                                       KC_LSFT,KC_BSPC,                        KC_ENT,  KC_SPC,
                                                       // Middle thumb keys
                                       KC_LGUI,KC_LALT,                        KC_LCTL, KC_LALT,
                                                       // Inboard thumb keys
                                       _______,_______,                        _______, _______
-  ),
-
-  [_LOWER] = LAYOUT_5x6(
-
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                             _______,KC_PSCR,            _______, KC_P0,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
-
   ),
 
   [_RAISE] = LAYOUT_5x6(
@@ -62,15 +47,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     dprintf("Highest layer: %d\n", get_highest_layer(state));
 
     switch (highest) {
-        case _LOWER:
-            backlight_disable();
-            break;
         case _RAISE:
-            backlight_enable();
             breathing_disable();
             break;
         default:
-            backlight_enable();
             breathing_enable();
     }
 
