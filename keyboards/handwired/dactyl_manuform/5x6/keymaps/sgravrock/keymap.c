012e7b127d9198ede7f5558caaf646509a2c4080
Steve Gravrock <sdg@panix.com>
2021-01-18 15:54:04 -0800

    Merged _FN layer back into _RAISE
    * No longer needed because brackets no longer conflict with fkeys
    * Having a raise key on the right makes ~/` easier to type

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/sgravrock/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/sgravrock/keymap.c
@@ -6,7 +6,6 @@
 
 #define _QWERTY 0
 #define _RAISE 1
-#define _FN 2
 
 #define RESET_MOD_MASK (MOD_MASK_GUI && MOD_MASK_CTRL && MOD_MASK_ALT)
 
@@ -17,37 +16,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_EQL , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
      KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-  TT(_RAISE), KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,TT(_FN),
+  TT(_RAISE), KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,TT(_RAISE),
                       KC_LEFT,KC_RIGHT,                                                        KC_UP,KC_DOWN,
                                                       // Outboard thumb keys
                                       KC_LSFT,KC_BSPC,                        KC_ENT,  KC_SPC,
                                                       // Middle thumb keys
                                       KC_LGUI,KC_LALT,                        KC_LGUI, KC_LCTL,
                                                       // Inboard thumb keys
-                                      KC_END ,KC_HOME,                        KC_PGUP, KC_PGDN
+                                      KC_END ,KC_HOME,                        KC_LBRC, KC_RBRC
   ),
 
   [_RAISE] = LAYOUT_5x6(
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,KC_LBRC,KC_RBRC,_______,
+       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_MUTE,
       KC_GRAVE,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,KC_DOWN,KC_RGHT,
                       _______,_______,                                                         _______,KC_LEFT,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
-                                               KC_VOLD,KC_VOLU,            _______,_______
+                                               KC_VOLD,KC_VOLU,            KC_PGUP,KC_PGDN
   ),
 
-  [_FN] = LAYOUT_5x6(
-       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_MUTE,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-                      _______,_______,                                                         _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______
-  ),
 };
 
 void keyboard_post_init_user(void) {
@@ -70,14 +59,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
     switch (highest) {
         case _RAISE:
-            backlight_enable();
             breathing_disable();
             break;
-        case _FN:
-            backlight_disable();
-            break;
         default:
-            backlight_enable();
             breathing_enable();
     }
 
